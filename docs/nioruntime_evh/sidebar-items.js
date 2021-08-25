241967e48ccc7ccf12ba5548a0d26aa98f1ff948
bitcoinmw <vsts@fv-az27-783.lccanjf0rsmetpz5jed3o1lmhd.dx.internal.cloudapp.net>
2021-08-25 22:52:29 +0000

    Pipelines-Bot: Updated site via 2ce27c30fa8941889780ddaa3dc5918450119384

--- docs/nioruntime_evh/sidebar-items.js
+++ docs/nioruntime_evh/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["eventhandler",""]]});
+initSidebarItems({"struct":[["EventHandler","EventHandler struct."],["WriteHandle","A handle that is associated with a particular connection and may be used for writing to the socket."]]});
